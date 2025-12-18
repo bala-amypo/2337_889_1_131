9fe30a727398c6ad5e214ee476052d10b889995e
bala-amypo <bala@amypo.in>
2025-12-18 16:05:23 +0000

    User commit

--- src/main/java/com/example/demo/entity/AppUser.java
+++ src/main/java/com/example/demo/entity/AppUser.java
@@ -1,5 +1,53 @@
 package com.example.demo.entity;
 
+import java.time.LocalDate;
+
 public class AppUser{
      private Integer id;
-     private LocalDate resto
+     private LocalDate restoredAt;
+     private Long eventId;
+     private String notes;
+
+     public AppUser(Integer id, LocalDate restoredAt, Long eventId, String notes) {
+        this.id = id;
+        this.restoredAt = restoredAt;
+        this.eventId = eventId;
+        this.notes = notes;
+     }
+
+     public Integer getId() {
+         return id;
+     }
+
+     public void setId(Integer id) {
+         this.id = id;
+     }
+
+     public LocalDate getRestoredAt() {
+         return restoredAt;
+     }
+
+     public void setRestoredAt(LocalDate restoredAt) {
+         this.restoredAt = restoredAt;
+     }
+
+     public Long getEventId() {
+         return eventId;
+     }
+
+     public void setEventId(Long eventId) {
+         this.eventId = eventId;
+     }
+
+     public String getNotes() {
+         return notes;
+     }
+
+     public void setNotes(String notes) {
+         this.notes = notes;
+     }
+
+
+
+
+}
