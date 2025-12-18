f0f6bf7c105e13b466384e6009e8b66752871614
bala-amypo <bala@amypo.in>
2025-12-18 16:35:22 +0000

    User commit

--- src/main/java/com/example/demo/controller/AppUserController.java
+++ src/main/java/com/example/demo/controller/AppUserController.java
@@ -15,6 +15,7 @@ import com.example.demo.entity.AppUser;
 import com.example.demo.service.AppUserService;
 
 @RestController
+@Rest
 public class AppUserController {
     @Autowired
     AppUserService src;
