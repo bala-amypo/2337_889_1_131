1e802a49285fb803f92863d24ef5e730a845a93b
bala-amypo <bala@amypo.in>
2025-12-18 08:59:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/DemandReading.java
+++ src/main/java/com/example/demo/entity/DemandReading.java
@@ -1,8 +1,38 @@
 package com.example.demo.entity;
 
+
+import java.time.LocalDate;
+
 public class DemandReading {
     @Id
     private Long id;
     private Double demandMW;
     private LocalDate recordedAt;
+
+    
+    public DemandReading(Long id, Double demandMW, LocalDate recordedAt) {
+        this.id = id;
+        this.demandMW = demandMW;
+        this.recordedAt = recordedAt;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Double getDemandMW() {
+        return demandMW;
+    }
+    public void setDemandMW(Double demandMW) {
+        this.demandMW = demandMW;
+    }
+    public LocalDate getRecordedAt() {
+        return recordedAt;
+    }
+    public void setRecordedAt(LocalDate recordedAt) {
+        this.recordedAt = recordedAt;
+    }
+
+
 }
