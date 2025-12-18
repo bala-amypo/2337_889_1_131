2d0a544d682c8f55c129cccdacc3f42fa5e43a0e
bala-amypo <bala@amypo.in>
2025-12-18 08:34:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/Zone.java
+++ src/main/java/com/example/demo/entity/Zone.java
@@ -2,4 +2,10 @@ package com.example.demo.entity;
 
 public class Zone{
     private Long id;
+    private String zonename;
+    private Integer priorityLevel;
+    private Integer population;
+    private Boolean active;
+    private Timestamp createdAt;
+    private Timestamp updateAt; 
 }
