7e2114b64d2085d6592a4dfa2133e57e9dec39eb
bala-amypo <bala@amypo.in>
2025-12-18 09:24:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/LoadSheddingEvent.java
+++ src/main/java/com/example/demo/entity/LoadSheddingEvent.java
@@ -2,4 +2,9 @@ package com.example.demo.entity;
 
 public class LoadSheddingEvent{
     @Id
+    private Long id;
+
+    private LocalDate eventStart;
+    private LocalDate eventEnd;
+    private 
 }
