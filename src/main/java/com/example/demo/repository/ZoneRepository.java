351d5e857e8a5eb625573f0365297d2e16f6d922
bala-amypo <bala@amypo.in>
2025-12-20 17:43:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/ZoneRepository.java
+++ src/main/java/com/example/demo/repository/ZoneRepository.java
@@ -3,7 +3,14 @@ package com.example.demo.repository;
 import com.example.demo.entity.Zone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ZoneRepository extends JpaRepository<Zone, Long> {
+    // Used for uniqueness checks
+    Optional<Zone> findByZoneName(String zoneName);
+    
+    // Used by LoadSheddingService to find candidate zones
+    List<Zone> findByActiveTrueOrderByPriorityLevelAsc();
 }
