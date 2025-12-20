639826905322e24c703656d13df9ea13c76617a0
bala-amypo <bala@amypo.in>
2025-12-20 16:58:35 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -1,6 +1,19 @@
-package com.example.demo.dto;
+package com.example.demo.entity;
 
-public class AuthRequest {
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder // Fixes builder()
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String email;
     private String password;
+    private String role;
+    private boolean active;
 }
--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,8 +1,15 @@
 package com.example.demo.dto;
 
-public class AuthResponse {
-    private String token;
-    private Long userId;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Fixes getEmail() and getPassword()
+@Builder // Fixes builder()
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequest {
     private String email;
-    private String role;
+    private String password;
 }
