dd2eddbc5ea56cfe4da87b21fb36c32d46b2420b
bala-amypo <bala@amypo.in>
2025-12-21 06:40:47 +0000

    User commit

--- src/main/java/com/example/demo/entity/AppUser.java
+++ src/main/java/com/example/demo/entity/AppUser.java
@@ -2,76 +2,24 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+
 @Entity
-@Setter
 @Getter
-@Data
+@Setter
 @Builder
-
-
+@NoArgsConstructor
 public class AppUser {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true)
     private String email;
 
-    @Column(nullable = false)
     private String password;
 
     private String role;
 
-    private Boolean active = true;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    public AppUser(Long id, String email, String password, String role, Boolean active) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-        this.active = active;
-    }
-
-    
-
+    private Boolean active;
 }
