32936224657a288815c2c8aea0aa919088590b8a
bala-amypo <bala@amypo.in>
2025-12-21 17:34:59 +0000

    User commit

--- src/main/java/com/example/demo/dto/ZoneDTO.java
+++ src/main/java/com/example/demo/dto/ZoneDTO.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ZoneDTO {
     private Long id;
-    private String name;
+    private String zoneName;
     private String description;
     private Boolean active;
     private Integer priorityLevel;
