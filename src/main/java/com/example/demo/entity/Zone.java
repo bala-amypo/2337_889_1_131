dc59eccb215d54284de4e678828e1735442b7019
bala-amypo <bala@amypo.in>
2025-12-23 08:07:10 +0000

    User commit

--- src/main/java/com/example/demo/entity/Zone.java
+++ src/main/java/com/example/demo/entity/Zone.java
@@ -13,7 +13,7 @@ public class Zone {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)bn9l
+    @Column(unique = true, nullable = false)
     private String zoneName;
 
     @Column(nullable = false)
